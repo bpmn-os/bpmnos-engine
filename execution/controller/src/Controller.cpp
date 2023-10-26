ceec71b7e058a7cb70d478049cdd7bd721fa167e
Asvin Goel <goel@telematique.eu>
2023-10-26 10:55:19 +0200

    Add controller base class

---
+++ execution/controller/src/Controller.cpp
@@ -0,0 +1,13 @@
+#include "Controller.h"
+
+using namespace BPMNOS::Execution;
+
+Controller::Controller()
+{
+}
+
+void Controller::connect(Engine* engine) {
+  engine->addListener(this);
+  engine->addEventHandler(this);
+}
+
---
+++ execution/controller/src/Controller.h
@@ -0,0 +1,24 @@
+#ifndef BPMNOS_Execution_Controller_H
+#define BPMNOS_Execution_Controller_H
+
+#include <bpmn++.h>
+#include "execution/engine/src/EventHandler.h"
+#include "execution/engine/src/Engine.h"
+#include "execution/listener/src/Listener.h"
+
+namespace BPMNOS::Execution {
+
+/**
+ * @brief Base class for an execution controller which dispatches events
+ * to the engine and listens to notification from the engine. 
+ */
+class Controller : public EventHandler, public Listener {
+public:
+  Controller();
+  virtual void connect(Engine* engine);
+};
+
+} // namespace BPMNOS::Execution
+
+#endif // BPMNOS_Execution_Controller_H
+
