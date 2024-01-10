8261eab20e57b58075981a2e66ce65cfa6335e1f
Asvin Goel <goel@telematique.eu>
2024-01-10 11:26:21 +0700

    Added test

--- tests/execution/compensation/test.h
+++ tests/execution/compensation/test.h
@@ -42,6 +42,56 @@ SCENARIO( "Compensate throw event without compensations", "[compensation][throwe
   }
 }
 
+SCENARIO( "Task with unused compensation task", "[compensation][task]" ) {
+  const std::string modelFile = "execution/compensation/Unused_compensation_task.bpmn";
+  REQUIRE_NOTHROW( Model::Model(modelFile) );
+  GIVEN( "A single instance with no input values" ) {
+
+    std::string csv =
+      "PROCESS_ID, INSTANCE_ID, ATTRIBUTE_ID, VALUE\n"
+      "Process_1, Instance_1,,\n"
+    ;
+
+    Model::StaticDataProvider dataProvider(modelFile,csv);
+    auto scenario = dataProvider.createScenario();
+
+    WHEN( "The engine is started with a recorder" ) {
+      Execution::Engine engine;
+      Execution::ReadyHandler readyHandler;
+      Execution::InstantEntryHandler entryHandler;
+      Execution::DeterministicTaskCompletionHandler completionHandler;
+      Execution::InstantExitHandler exitHandler;
+      Execution::TimeWarp timeHandler;
+      engine.addEventHandler(&readyHandler);
+      engine.addEventHandler(&entryHandler);
+      engine.addEventHandler(&completionHandler);
+      engine.addEventHandler(&exitHandler);
+      engine.addEventHandler(&timeHandler);
+      Execution::Recorder recorder;
+//      Execution::Recorder recorder(std::cerr);
+      engine.addListener(&recorder);
+      engine.run(scenario.get());
+      THEN( "Then the nodes change their states in the correct order" ) {
+        auto entryLog = recorder.find(nlohmann::json{{"state", "ENTERED"}});
+        REQUIRE( entryLog[0]["nodeId"] == nullptr );
+        REQUIRE( entryLog[1]["nodeId"] == "StartEvent_1" );
+        REQUIRE( entryLog[2]["nodeId"] == "Activity_1" );
+        REQUIRE( entryLog[3]["nodeId"] == "EndEvent_1" );
+
+        auto busyLog = recorder.find(nlohmann::json{{"state", "BUSY"}});
+        REQUIRE( busyLog[0]["nodeId"] == nullptr );
+        REQUIRE( busyLog[1]["nodeId"] == "Activity_1" );
+        REQUIRE( busyLog[2]["nodeId"] == "CompensateBoundaryEvent_1" );
+
+        auto completionLog = recorder.find(nlohmann::json{{"state", "COMPLETED"}});
+        REQUIRE( completionLog[0]["nodeId"] == "Activity_1" );
+        REQUIRE( completionLog[1]["nodeId"] == nullptr );
+     }
+    }
+  }
+}
+
+
 SCENARIO( "Task with compensation task", "[compensation][task]" ) {
   const std::string modelFile = "execution/compensation/Compensation_task.bpmn";
   REQUIRE_NOTHROW( Model::Model(modelFile) );
