0fa235a184341cd7611c39c635df4f97b3c377a6
Asvin Goel <goel@telematique.eu>
2024-02-27 19:53:02 +0100

    Bugfix

--- tests/examples/assignment_problem/test.h
+++ tests/examples/assignment_problem/test.h
@@ -1,6 +1,6 @@
 SCENARIO( "Assignment problem", "[examples][assignment_problem]" ) {
   const std::string modelFile = "examples/assignment_problem/Assignment_problem.bpmn";
-  BPMNOS::Model::LookupTable::folders = { std::string(std::filesystem::current_path()) + "/xamples/assignment_problem" };
+  BPMNOS::Model::LookupTable::folders = { std::string(std::filesystem::current_path()) + "/examples/assignment_problem" };
   REQUIRE_NOTHROW( Model::Model(modelFile) );
 
   GIVEN( "One client and one server" ) {
