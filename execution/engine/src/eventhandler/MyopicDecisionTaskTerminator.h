73097d79456e612f4c33a2d04e2c7cf7f1014f62
Asvin Goel <goel@telematique.eu>
2024-03-03 19:43:13 +0100

    Update docs

--- execution/engine/src/eventhandler/MyopicDecisionTaskTerminator.h
+++ execution/engine/src/eventhandler/MyopicDecisionTaskTerminator.h
@@ -9,8 +9,8 @@ namespace BPMNOS::Execution {
 /**
  * @brief Class creating an error event for a token at a decision task if no choice has been made.
  *
- * The MyopicDecisionTaskTerminator terminates a @ref BPMNOS:Model::DecsionsTask with an error if
- * no choice has been made previously. It assumes that a choice handler making such choices has been called before.
+ * The MyopicDecisionTaskTerminator terminates a @ref BPMNOS:Model::DecisionTask with an error if
+ * no choice has been made previously. It assumes that a handler making potential choices has been called before.
  * The handler is myopic and does not consider that an increase in the timestamp may affect possible choices.
  */
 class MyopicDecisionTaskTerminator : public EventHandler {
