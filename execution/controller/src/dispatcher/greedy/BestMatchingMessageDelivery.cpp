38c951edf41ffb5ddb35bf8c7ff3822dc059f7c3
Asvin Goel <goel@telematique.eu>
2024-03-20 12:43:04 +0100

    Added BestMatchingMessageDelivery prototype

---
+++ execution/controller/src/dispatcher/greedy/BestMatchingMessageDelivery.cpp
@@ -0,0 +1,76 @@
+#include "BestMatchingMessageDelivery.h"
+#include "execution/engine/src/Mediator.h"
+#include "execution/controller/src/decisions/MessageDeliveryDecision.h"
+#include "model/parser/src/extensionElements/MessageDefinition.h"
+#include <cassert>
+
+using namespace BPMNOS::Execution;
+
+BestMatchingMessageDelivery::BestMatchingMessageDelivery()
+{
+}
+
+void BestMatchingMessageDelivery::connect(Mediator* mediator) {
+  mediator->addSubscriber(this, 
+    Execution::Observable::Type::MessageDeliveryRequest,
+    Execution::Observable::Type::Message
+  );
+  EventDispatcher::connect(mediator);
+}
+
+void BestMatchingMessageDelivery::notice(const Observable* observable) {
+  if ( observable->getObservableType() == Observable::Type::MessageDeliveryRequest ) {
+    auto request = static_cast<const DecisionRequest*>(observable);
+    assert(request->token->node);
+    auto recipientHeader = request->token->node->extensionElements->as<BPMNOS::Model::ExtensionElements>()->messageDefinitions.front()->getRecipientHeader(request->token->status);
+    auto senderCandidates = request->token->node->extensionElements->as<BPMNOS::Model::ExtensionElements>()->messageCandidates;
+    auto_list< std::weak_ptr<const Message>, std::shared_ptr<MessageDeliveryDecision> > candidates;
+    // determine candidate messages
+    for ( auto& [ message_ptr ] : messages ) {
+      if( auto message = message_ptr.lock();
+        message &&
+        std::ranges::contains(senderCandidates, message->origin) &&
+        message->matches(recipientHeader)
+      ) {
+        candidates.emplace_back( message->weak_from_this(), std::make_unique<MessageDeliveryDecision>(request->token, message.get()) );
+      }
+    }
+    messageDeliveryRequests.emplace_back(request->token->weak_from_this(), request->weak_from_this(), candidates, recipientHeader);
+  }
+  else if ( observable->getObservableType() == Observable::Type::Message ) {
+    auto message = static_cast<const Message*>(observable);
+    if ( message->state == Message::State::CREATED ) {
+      messages.emplace_back( message->weak_from_this() );
+      // add new message to relevant candidate lists
+      auto recipientCandidates = message->origin->extensionElements->as<BPMNOS::Model::ExtensionElements>()->messageCandidates;
+      for ( auto& [token_ptr, request_ptr, candidates, recipientHeader ] : messageDeliveryRequests ) {
+        if ( auto token = token_ptr.lock();
+          token &&
+          std::ranges::contains(recipientCandidates, token->node) &&
+          message->matches(recipientHeader)
+        ) {
+          candidates.emplace_back( message->weak_from_this(), std::make_unique<MessageDeliveryDecision>(token.get(), message) );
+        }
+      }
+    }
+  }
+  else {
+    assert(!"Unexpected observable type");
+  }
+}
+
+std::shared_ptr<Event> BestMatchingMessageDelivery::dispatchEvent( [[maybe_unused]] const SystemState* systemState ) {
+  for ( auto& [token_ptr, request_ptr, candidates, recipientHeader ] : messageDeliveryRequests ) {
+    if( auto token = token_ptr.lock() )  {
+      if ( request_ptr.lock() )  {
+        for ( auto& [message_ptr, decision] : candidates ) {
+          if ( auto message = message_ptr.lock() ) {
+            return decision;
+          }
+        }
+      }
+    }
+  }
+  return nullptr;
+}
+
---
+++ execution/controller/src/dispatcher/greedy/BestMatchingMessageDelivery.h
@@ -0,0 +1,29 @@
+#ifndef BPMNOS_Execution_BestMatchingMessageDelivery_H
+#define BPMNOS_Execution_BestMatchingMessageDelivery_H
+
+#include <bpmn++.h>
+#include "execution/engine/src/EventDispatcher.h"
+#include "execution/engine/src/Observer.h"
+#include "execution/engine/src/Message.h"
+#include "execution/controller/src/decisions/MessageDeliveryDecision.h"
+
+namespace BPMNOS::Execution {
+
+/**
+ * @brief Class creating an entry event for a token awaiting the entry at a regular activity (i.e. not a job).
+ */
+class BestMatchingMessageDelivery : public EventDispatcher, public Observer {
+public:
+  BestMatchingMessageDelivery();
+  std::shared_ptr<Event> dispatchEvent( const SystemState* systemState ) override;
+  void connect(Mediator* mediator) override;
+  void notice(const Observable* observable) override;
+private:
+  auto_list< std::weak_ptr<const Token>, std::weak_ptr<const DecisionRequest>, auto_list< std::weak_ptr<const Message>, std::shared_ptr<MessageDeliveryDecision> >, const BPMNOS::Values > messageDeliveryRequests;
+  auto_list< std::weak_ptr<const Message> > messages;
+};
+
+} // namespace BPMNOS::Execution
+
+#endif // BPMNOS_Execution_BestMatchingMessageDelivery_H
+
