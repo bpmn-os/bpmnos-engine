9efd2cc1945a91f3625d8285b521896863250064
Asvin Goel <goel@telematique.eu>
2024-03-31 21:36:58 +0200

    Move instance attribute to data object

--- tests/model/parser/test.h
+++ tests/model/parser/test.h
@@ -2,22 +2,6 @@ TEST_CASE( "Parse BPMN elements", "[model][parser]" ) {
   REQUIRE_NOTHROW( Model::Model(std::string("model/parser/Model_without_extensions.bpmn")) );
 }
 
-TEST_CASE( "Parse empty custom activity", "[model][parser]" ) {
-  REQUIRE_NOTHROW( Model::Model(std::string("model/parser/EmptyCustomActivity.bpmn")) );
-}
-
-TEST_CASE( "Parse request activity", "[model][parser]" ) {
-  REQUIRE_NOTHROW( Model::Model(std::string("model/parser/RequestActivity.bpmn")) );
-}
-
-TEST_CASE( "Parse release activity", "[model][parser]" ) {
-  REQUIRE_NOTHROW( Model::Model(std::string("model/parser/ReleaseActivity.bpmn")) );
-}
-
-TEST_CASE( "Parse resource activity", "[model][parser]" ) {
-  REQUIRE_NOTHROW( Model::Model(std::string("model/parser/ResourceActivity.bpmn")) );
-}
-
 TEST_CASE( "Parse message flows", "[model][parser]" ) {
   Model::Model model(std::string("model/parser/Messaging.bpmn"));
   std::unordered_map< std::string, std::set< std::string > > candidates;
