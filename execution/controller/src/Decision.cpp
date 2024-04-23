be582966ca9e6b0da8d2d2c2f066db2c43a9a54c
Asvin Goel <goel@telematique.eu>
2024-04-23 19:31:30 +0200

    Fix dependencies

--- execution/controller/src/Decision.cpp
+++ execution/controller/src/Decision.cpp
@@ -11,7 +11,7 @@ Decision::Decision(Evaluator* evaluator)
 
 void Decision::determineDependencies(const std::set<const BPMNOS::Model::Attribute*>& dependencies) {
   for ( auto attribute : dependencies ) {
-    if ( attribute->category == BPMNOS::Model::Attribute::Category::DATA ) {
+    if ( attribute->category != BPMNOS::Model::Attribute::Category::STATUS ) {
       dataDependencies.insert(attribute);
     }
     else if ( attribute->index == BPMNOS::Model::ExtensionElements::Index::Timestamp ) {
