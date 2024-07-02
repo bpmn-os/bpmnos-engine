ac4df2d92029aa345e30cdc7465db2a4565b9830
Asvin Goel <goel@telematique.eu>
2024-07-02 09:40:26 +0200

    Added if-then-else expression test

--- tests/model/expression/test.h
+++ tests/model/expression/test.h
@@ -151,3 +151,93 @@ SCENARIO( "Generic expression", "[model][expression]" ) {
     }
   }
 }
+
+SCENARIO( "Another generic expression", "[model][expression]" ) {
+  const std::string modelFile = "tests/model/expression/anotherGenericExpression.bpmn";
+  REQUIRE_NOTHROW( Model::Model(modelFile) );
+
+  GIVEN( "An expression if ( x )  ( y >= 3 ); else 5;" ) {
+    WHEN( "The expression is executed with x = 8, y = 15" ) {
+      std::string csv =
+        "PROCESS_ID, INSTANCE_ID, ATTRIBUTE_ID, VALUE\n"
+        "Process_1, Instance_1,X,8\n"
+        "Process_1, Instance_1,Y,15\n"
+      ;
+      Model::StaticDataProvider dataProvider(modelFile,csv);
+
+      auto scenario = dataProvider.createScenario();
+      auto instantiations = scenario->getCurrentInstantiations(0);
+
+      THEN( "The result is correct" ) {
+        Values globals;
+        for ( auto& [process,status,data] : instantiations ) {
+          auto extensionElements = process->extensionElements->represents<Model::ExtensionElements>();
+          extensionElements->applyOperators(status,data,globals);
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].has_value() == true );
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].value() == 1 );
+        }
+      }
+    }
+    WHEN( "The expression is executed with x = 8, y = 15" ) {
+      std::string csv =
+        "PROCESS_ID, INSTANCE_ID, ATTRIBUTE_ID, VALUE\n"
+        "Process_1, Instance_1,X,8\n"
+        "Process_1, Instance_1,Y,0\n"
+      ;
+      Model::StaticDataProvider dataProvider(modelFile,csv);
+
+      auto scenario = dataProvider.createScenario();
+      auto instantiations = scenario->getCurrentInstantiations(0);
+
+      THEN( "The result is correct" ) {
+        Values globals;
+        for ( auto& [process,status,data] : instantiations ) {
+          auto extensionElements = process->extensionElements->represents<Model::ExtensionElements>();
+          extensionElements->applyOperators(status,data,globals);
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].has_value() == true );
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].value() == 0 );
+        }
+      }
+    }
+    WHEN( "The expression is executed with x = 0, y = undefined" ) {
+      std::string csv =
+        "PROCESS_ID, INSTANCE_ID, ATTRIBUTE_ID, VALUE\n"
+        "Process_1, Instance_1,X,0\n"
+      ;
+      Model::StaticDataProvider dataProvider(modelFile,csv);
+
+      auto scenario = dataProvider.createScenario();
+      auto instantiations = scenario->getCurrentInstantiations(0);
+
+      THEN( "The result is correct" ) {
+        Values globals;
+        for ( auto& [process,status,data] : instantiations ) {
+          auto extensionElements = process->extensionElements->represents<Model::ExtensionElements>();
+          extensionElements->applyOperators(status,data,globals);
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].has_value() == false ); // all inputs must be defined, even if they are not used in the end
+        }
+      }
+    }
+    WHEN( "The expression is executed with x = 0, y = 0" ) {
+      std::string csv =
+        "PROCESS_ID, INSTANCE_ID, ATTRIBUTE_ID, VALUE\n"
+        "Process_1, Instance_1,X,0\n"
+        "Process_1, Instance_1,Y,0\n"
+      ;
+      Model::StaticDataProvider dataProvider(modelFile,csv);
+
+      auto scenario = dataProvider.createScenario();
+      auto instantiations = scenario->getCurrentInstantiations(0);
+
+      THEN( "The result is correct" ) {
+        Values globals;
+        for ( auto& [process,status,data] : instantiations ) {
+          auto extensionElements = process->extensionElements->represents<Model::ExtensionElements>();
+          extensionElements->applyOperators(status,data,globals);
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].has_value() == true );
+          REQUIRE( status[extensionElements->attributeRegistry["z"]->index].value() == 5 );
+        }
+      }
+    }
+  }
+}
