361dd0a9b7d95f20cedb2043d867f31faa1bcd93
Asvin Goel <github@telematique.eu>
2025-01-03 14:47:35 +0100

    Started refactoring expressions

--- model/bpmnos/src/extensionElements/Expression.cpp
+++ model/bpmnos/src/extensionElements/Expression.cpp
@@ -1,44 +1,133 @@
 #include "Expression.h"
-#include "model/bpmnos/src/extensionElements/expression/LinearExpression.h"
-#include "model/bpmnos/src/extensionElements/expression/GenericExpression.h"
-#include "model/bpmnos/src/extensionElements/expression/NullCondition.h"
-#include "model/bpmnos/src/extensionElements/expression/Enumeration.h"
-#include "model/bpmnos/src/extensionElements/expression/StringExpression.h"
+#include "model/utility/src/CollectionRegistry.h"
+#include "model/utility/src/Keywords.h"
 
 using namespace BPMNOS::Model;
 
-Expression::Expression(XML::bpmnos::tParameter* parameter, const AttributeRegistry& attributeRegistry)
+Expression::Expression(std::string expression, const AttributeRegistry& attributeRegistry)
   : attributeRegistry(attributeRegistry)
-  , expression(parameter->value.value().get().value)
+  , expression(expression)
+  , compiled(LIMEX::Expression<double>(expression))
+  , type(getType())
 {
+  if ( auto name = compiled.getTarget(); name.has_value() ) {
+    if ( name.value() == BPMNOS::Keyword::Undefined ) {
+      throw std::runtime_error("Expression: illegal assignment '" + expression +"'");
+    }
+    target = attributeRegistry[ name.value() ];
+  }
+  
+  for ( auto& name : compiled.getVariables() ) {
+    if ( name != BPMNOS::Keyword::Undefined ) {
+      auto attribute = attributeRegistry[ name ];
+      inputs.push_back(attribute);
+      variables.push_back(attribute);
+    }
+  }
+  for ( auto& name : compiled.getCollections() ) {
+    if ( name == BPMNOS::Keyword::Undefined ) {
+      throw std::runtime_error("Expression: illegal expression '" + expression +"'");
+    }
+    auto attribute = attributeRegistry[ name ];
+    inputs.push_back(attribute);
+    collections.push_back(attribute);
+  }
 }
 
-std::unique_ptr<Expression> Expression::create(XML::bpmnos::tParameter* parameter, const AttributeRegistry& attributeRegistry) {
-  if ( parameter->name.value.value == "linear" ) {
-    return std::make_unique<LinearExpression>(parameter, attributeRegistry);
+Expression::Type Expression::getType() {
+  auto& variableNames = compiled.getVariables();
+  if ( std::find( variableNames.begin(), variableNames.end(), BPMNOS::Keyword::Undefined ) != variableNames.end() ) {
+    // only lhs == undefined, lhs != undefined, and lhs := undefined are allowed
+    auto& root = compiled.getRoot(); 
+    assert( root.operands.size() == 1 );
+    assert( root.type == LIMEX::Type::group );
+    auto& node = std::get< LIMEX::Node<double> >(root.operands[0]);
+    
+    if ( node.type == LIMEX::Type::assign ) {
+      assert( node.operands.size() == 1 );
+      if ( 
+        !std::holds_alternative< LIMEX::Node<double> >(node.operands[0]) || 
+        std::get< LIMEX::Node<double> >(node.operands[0]).type != LIMEX::Type::variable
+      ) {
+        throw std::runtime_error("Expression: illegal assignment '" + expression +"'");
+      }
+      return Type::UNASSIGN;
+    }
+
+    if ( node.type != LIMEX::Type::equal_to && node.type != LIMEX::Type::not_equal_to ) {
+      throw std::runtime_error("Expression: illegal expression '" + expression +"'");
+    }
+    
+    assert( node.operands.size() == 2 );
+    assert( std::holds_alternative< LIMEX::Node<double> >(node.operands[0]) );
+    assert( std::holds_alternative< LIMEX::Node<double> >(node.operands[1]) );
+    auto& lhs = std::get< LIMEX::Node<double> >(node.operands[0]);
+    auto& rhs = std::get< LIMEX::Node<double> >(node.operands[1]);
+    assert( !lhs.operands.empty() );
+    assert( !rhs.operands.empty() );
+//  std::cout << node.operands.size() << std::endl; // Print the entire AST
+    if (
+      lhs.type != LIMEX::Type::variable ||
+      variableNames.at( std::get< size_t >(lhs.operands[0]) ) == BPMNOS::Keyword::Undefined ||
+      rhs.type != LIMEX::Type::variable ||
+      variableNames.at( std::get< size_t >(rhs.operands[0]) ) != BPMNOS::Keyword::Undefined
+    ) {
+      throw std::runtime_error("Expression: illegal comparison '" + expression +"'");
+    }
+
+    return node.type == LIMEX::Type::equal_to ? Type::IS_NULL : Type::IS_NOT_NULL;
   }
-  else if ( parameter->name.value.value == "generic" ) {
-    return std::make_unique<GenericExpression>(parameter, attributeRegistry);
-  }
-  else if ( parameter->name.value.value == "nullcondition" ) {
-    return std::make_unique<NullCondition>(parameter, attributeRegistry);
-  }
-  else if ( parameter->name.value.value == "enumeration" ) {
-    return std::make_unique<Enumeration>(parameter, attributeRegistry);
-  }
-  else if ( parameter->name.value.value == "string" ) {
-    return std::make_unique<StringExpression>(parameter, attributeRegistry);
-  }
-  else {
-    throw std::logic_error("Expression: illegal expression type");
-  }
+  return Type::OTHER;
 }
 
-std::pair< std::optional<BPMNOS::number>, std::optional<BPMNOS::number> > Expression::getBounds([[maybe_unused]] const Attribute* attribute, [[maybe_unused]] const Values& status, [[maybe_unused]] const Values& data, [[maybe_unused]] const BPMNOS::Values& globals) const {
-  return {std::nullopt,std::nullopt};
+template <typename DataType>
+std::optional<BPMNOS::number> Expression::execute(const BPMNOS::Values& status, const DataType& data, const BPMNOS::Values& globals) const {
+  if ( type == Type::UNASSIGN ) {
+    return std::nullopt;
+  }
+  if ( type == Type::IS_NULL ) {
+    assert(variables.size() == 1);
+    auto value = attributeRegistry.getValue(variables[0],status,data,globals);
+    return number( (double)!value.has_value() );
+  }
+  if ( type == Type::IS_NOT_NULL ) {
+    assert(variables.size() == 1);
+    auto value = attributeRegistry.getValue(variables[0],status,data,globals);
+    return number( (double)value.has_value() );
+  }
+  
+  // collect variable values
+  std::vector< double > variableValues;
+  for ( auto attribute : variables ) {
+    auto value = attributeRegistry.getValue(attribute,status,data,globals);
+    if ( !value.has_value() ) {
+      // return nullopt because required attribute value is not given
+      return std::nullopt;
+    }
+    variableValues.push_back( (double)value.value() );
+  }
+  
+  // collect values of all variables in collection
+  std::vector< std::vector< double > > collectionValues;
+  for ( auto attribute : collections ) {
+    collectionValues.push_back( {} );
+    auto collection = attributeRegistry.getValue(attribute,status,data,globals);
+    if ( !collection.has_value() ) {
+      // return nullopt because required collection is not given
+      return std::nullopt;
+    }
+    for ( auto value : collectionRegistry[(size_t)collection.value()].values ) {
+      if ( !value.has_value() ) {
+        // return nullopt because a value in required collection is missing
+        return std::nullopt;
+      }
+      collectionValues.back().push_back( (double)value.value() );
+    }
+  }
+  
+  return number(compiled.evaluate(variableValues,collectionValues));
 }
 
-std::pair< std::optional<BPMNOS::number>, std::optional<BPMNOS::number> > Expression::getBounds([[maybe_unused]] const Attribute* attribute, [[maybe_unused]] const Values& status, [[maybe_unused]] const SharedValues& data, [[maybe_unused]] const BPMNOS::Values& globals) const {
-  return {std::nullopt,std::nullopt};
-}
+template std::optional<BPMNOS::number> Expression::execute<BPMNOS::Values>(const BPMNOS::Values& status, const BPMNOS::Values& data, const BPMNOS::Values& globals) const;
+template std::optional<BPMNOS::number> Expression::execute<BPMNOS::SharedValues>(const BPMNOS::Values& status, const BPMNOS::SharedValues& data, const BPMNOS::Values& globals) const;
 
--- model/bpmnos/src/extensionElements/Expression.h
+++ model/bpmnos/src/extensionElements/Expression.h
@@ -1,34 +1,35 @@
 #ifndef BPMNOS_Model_Expression_H
 #define BPMNOS_Model_Expression_H
 
-#include <exprtk.hpp>
+#include <limex.h>
 
 #include "Attribute.h"
 #include "AttributeRegistry.h"
-#include "Parameter.h"
 #include "model/utility/src/Number.h"
 #include "model/utility/src/StringRegistry.h"
 
 namespace BPMNOS::Model {
 
 /**
- * @brief Abstract base class representing an operator that uses an expression to determine the value
- * of a status attribute.
+ * @brief Class representing a mathematical expression.
  **/
 class Expression {
 public:
-  Expression(XML::bpmnos::tParameter* parameter, const AttributeRegistry& attributeRegistry);
-  virtual ~Expression() = default;  // Virtual destructor
+  enum class Type { UNASSIGN, IS_NULL, IS_NOT_NULL, OTHER };
+  Expression(const std::string expression, const AttributeRegistry& attributeRegistry);
   const AttributeRegistry& attributeRegistry;
   const std::string expression;
-  static std::unique_ptr<Expression> create(XML::bpmnos::tParameter* parameter, const AttributeRegistry& attributeRegistry);
-  std::set<const Attribute*> inputs; ///< Vector containing all input attributes influencing the result of the expression.
+  const LIMEX::Expression<double> compiled;
+  const Type type;
+  std::optional<const Attribute*> target;
+  std::vector<const Attribute*> inputs; ///< Vector containing all input attributes and collections used by the expression.
+  std::vector<const Attribute*> variables; ///< Vector containing all input attributes used by the expression.
+  std::vector<const Attribute*> collections; ///< Vector containing all input collections used by the expression.
 
-  virtual std::optional<BPMNOS::number> execute(const Values& status, const Values& data, const BPMNOS::Values& globals) const = 0;
-  virtual std::optional<BPMNOS::number> execute(const Values& status, const SharedValues& data, const BPMNOS::Values& globals) const = 0;
-
-  virtual std::pair< std::optional<BPMNOS::number>, std::optional<BPMNOS::number> > getBounds(const Attribute* attribute, const Values& status, const Values& data, const BPMNOS::Values& globals) const;
-  virtual std::pair< std::optional<BPMNOS::number>, std::optional<BPMNOS::number> > getBounds(const Attribute* attribute, const Values& status, const SharedValues& data, const BPMNOS::Values& globals) const;
+  template <typename DataType>
+  std::optional<BPMNOS::number> execute(const BPMNOS::Values& status, const DataType& data, const BPMNOS::Values& globals) const;
+private:
+  Type getType();
 };
 
 } // namespace BPMNOS::Model
