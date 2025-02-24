6d8139947e5a456fbc1ba6804e32e1771c5c7bec
Asvin Goel <github@telematique.eu>
2025-02-24 13:16:14 +0100

    Added 2 instance test

--- tests/cpcontroller/process/test.h
+++ tests/cpcontroller/process/test.h
@@ -107,6 +107,41 @@ SCENARIO( "Simple executable process", "[cpcontroller][process]" ) {
       controller.subscribe(&engine); // only necessary to validate consistency of solution and identify errors
       Execution::TimeWarp timeHandler;
       timeHandler.connect(&engine);
+      Execution::Recorder recorder;
+//      Execution::Recorder recorder(std::cerr);
+      recorder.subscribe(&engine);
+      engine.run(scenario.get(),10);
+      
+//std::cerr << "Model:\n" << controller.getModel().stringify() << std::endl;
+//std::cerr << "Solution:\n" << solution.stringify() << std::endl;
+      REQUIRE( solution.complete() ); // requires subscription of controller to engine
+      REQUIRE( solution.errors().empty() ); // requires subscription of controller to engine
+    }
+  }
+
+  GIVEN( "Two instances with no input values" ) {
+    std::string csv =
+      "PROCESS_ID, INSTANCE_ID, ATTRIBUTE_ID, VALUE\n"
+      "Process_1, Instance_1,,\n"
+      "Process_1, Instance_2,,\n"
+    ;
+
+    Model::StaticDataProvider dataProvider(modelFile,csv);
+    auto scenario = dataProvider.createScenario();
+
+    REQUIRE_NOTHROW( BPMNOS::Execution::FlattenedGraph(scenario.get()) );
+
+    WHEN( "The model is created" ) {
+      Execution::GuidedEvaluator evaluator;
+      Execution::SeededGreedyController controller(scenario.get(), &evaluator);
+      controller.setSeed( Execution::CPSeed::defaultSeed( controller.getVertices().size() ) );
+
+      auto& solution = controller.createSolution();
+      Execution::Engine engine;
+      controller.connect(&engine);
+      controller.subscribe(&engine); // only necessary to validate consistency of solution and identify errors
+      Execution::TimeWarp timeHandler;
+      timeHandler.connect(&engine);
 //      Execution::Recorder recorder;
       Execution::Recorder recorder(std::cerr);
       recorder.subscribe(&engine);
@@ -118,5 +153,6 @@ std::cerr << "Solution:\n" << solution.stringify() << std::endl;
       REQUIRE( solution.errors().empty() ); // requires subscription of controller to engine
     }
   }
+
 };
 
