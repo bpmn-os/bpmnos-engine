904b0224d2e1a416ecb7df293aa8e4ceadb81643
Asvin Goel <github@telematique.eu>
2025-04-17 09:28:20 +0200

    Added test

--- tests/cpcontroller/examples/knapsack_problem/test.h
+++ tests/cpcontroller/examples/knapsack_problem/test.h
@@ -40,10 +40,10 @@ SCENARIO( "Knapsack problem", "[cpcontroller][knapsack_problem]" ) {
 //std::cerr << "Model:\n" << controller.getModel().stringify() << std::endl;
 //std::cerr << "Solution:\n" << solution.stringify() << std::endl;
 //std::cerr << "Errors:\n" << solution.errors() << std::endl;
-      THEN( "The solution is termiates" ) {
+      THEN( "The engine terminates prematurely" ) {
         auto terminationLog = recorder.find(nlohmann::json{{"event","termination"}});
         REQUIRE( !terminationLog.empty() );
-//        REQUIRE( solution.complete() );
+        REQUIRE( !solution.complete() );
 //        REQUIRE( solution.errors().empty() );
 //        REQUIRE( solution.getObjectiveValue().has_value() );
 //        REQUIRE( solution.getObjectiveValue().value() == engine.getSystemState()->getObjective() );
@@ -75,7 +75,56 @@ SCENARIO( "Knapsack problem", "[cpcontroller][knapsack_problem]" ) {
 //std::cerr << "Model:\n" << controller.getModel().stringify() << std::endl;
 //std::cerr << "Solution:\n" << solution.stringify() << std::endl;
 //std::cerr << "Errors:\n" << solution.errors() << std::endl;
-      THEN( "The solution is completes without errors" ) {
+      THEN( "The solution is complete and satisfies all constraints" ) {
+        auto terminationLog = recorder.find(nlohmann::json{{"event","termination"}});
+        REQUIRE( terminationLog.empty() );
+        REQUIRE( solution.complete() );
+        REQUIRE( solution.errors().empty() );
+        REQUIRE( solution.getObjectiveValue().has_value() );
+        REQUIRE( solution.getObjectiveValue().value() == engine.getSystemState()->getObjective() );
+      }
+    }
+  }
+
+  GIVEN( "One knapsack and three items provided in a suitable order" ) {
+
+    std::string csv =
+      "PROCESS_ID; INSTANCE_ID; ATTRIBUTE_ID; VALUE\n"
+      "ItemProcess;Item1;Weight;20\n"
+      "ItemProcess;Item1;Value;100\n"
+      "ItemProcess;Item2;Weight;15\n"
+      "ItemProcess;Item2;Value;50\n"
+      "ItemProcess;Item3;Weight;22\n"
+      "ItemProcess;Item3;Value;120\n"
+      "KnapsackProcess;Knapsack1;Capacity;40\n"
+    ;
+    
+    WHEN( "The engine is started with the default seed" ) {
+
+      Model::StaticDataProvider dataProvider(modelFile,csv);
+      auto scenario = dataProvider.createScenario();
+ 
+      REQUIRE_NOTHROW( Execution::FlattenedGraph(scenario.get()) );
+
+      Execution::GuidedEvaluator evaluator;
+      Execution::SeededGreedyController controller(scenario.get(), &evaluator);
+      controller.setSeed( Execution::CPSeed::defaultSeed( controller.getVertices().size() ) );
+
+      [[maybe_unused]] auto& solution = controller.createSolution();
+      Execution::Engine engine;
+      controller.connect(&engine);
+      controller.subscribe(&engine); 
+      Execution::TimeWarp timeHandler;
+      timeHandler.connect(&engine);
+      Execution::Recorder recorder;
+//      Execution::Recorder recorder(std::cerr);
+      recorder.subscribe(&engine);
+      engine.run(scenario.get(),10);
+
+//std::cerr << "Model:\n" << controller.getModel().stringify() << std::endl;
+//std::cerr << "Solution:\n" << solution.stringify() << std::endl;
+//std::cerr << "Errors:\n" << solution.errors() << std::endl;
+      THEN( "The solution is complete and satisfies all constraints" ) {
         auto terminationLog = recorder.find(nlohmann::json{{"event","termination"}});
         REQUIRE( terminationLog.empty() );
         REQUIRE( solution.complete() );
