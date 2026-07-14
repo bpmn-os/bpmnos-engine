8699cb1ec414d2f5596df5e0ffef4857282902ab
Asvin Goel <github@telematique.eu>
2026-07-14 17:42:09 +0200

    Use steady_clock

--- execution/controller/src/dispatcher/Metronome.cpp
+++ execution/controller/src/dispatcher/Metronome.cpp
@@ -11,13 +11,13 @@ Metronome::Metronome(unsigned int clockTickDuration)
 
 void Metronome::initialize(unsigned int clockTickDuration) {
   this->clockTickDuration = clockTickDuration;
-  timestamp = std::chrono::system_clock::now();
+  timestamp = std::chrono::steady_clock::now();
 }
 
 std::shared_ptr<Event> Metronome::dispatchEvent( const SystemState* systemState ) {
   auto trigger = timestamp + std::chrono::milliseconds( clockTickDuration );
   std::this_thread::sleep_until(trigger);
-  timestamp = std::chrono::system_clock::now();
+  timestamp = std::chrono::steady_clock::now();
 
   return std::make_shared<ClockTickEvent>(systemState);
 }
